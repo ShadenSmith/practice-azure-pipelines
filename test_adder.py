31cdaf9b81c163f6ad8bde52d976e894d8972fa0
Shaden Smith <Shaden.Smith@microsoft.com>
2020-05-05 21:50:23 +0000

    pytest

--- test_adder.py
+++ test_adder.py
@@ -1,34 +1,21 @@
-
-import unittest
-
 import torch
 import adder
 import pathlib
 
-class TestAdder(unittest.TestCase):
-    def test_add(self):
-        result = adder.add(2,3)
-        self.assertTrue(result == 5)
+def test_add():
+    assert adder.add(2,3) == 5
 
-    def test_add2(self):
-        result = adder.add(3, -3)
-        self.assertTrue(result == 0)
+def test_add2():
+    assert adder.add(3, -3) == 0
 
-    def test_add3(self):
-        result = adder.add3(1, 2, 3)
-        self.assertTrue(result == 6)
+def test_add3():
+    assert adder.add3(1, 2, 3) == 6
 
-    def test_gpu(self):
-        pass
-        #self.assertTrue(torch.cuda.is_available())
+def test_gpu():
+    #assert torch.cuda.is_available()
+    pass
 
-    def test_artifact(self):
-        pathlib.Path('logs').mkdir(parents=True, exist_ok=True)
-        with open('logs/test_output.log', 'w') as fout:
-            print('Test output', file=fout)
-
-
-if __name__ == '__main__':
-    unittest.main()
-
-
+def test_artifact():
+    pathlib.Path('logs').mkdir(parents=True, exist_ok=True)
+    with open('logs/test_output.log', 'w') as fout:
+        print('Test output', file=fout)
